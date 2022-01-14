9dfad10f0e56b094ee090bce2c2b13a69fd29291
CharlieAprog <charlie.albietz.uni@gmail.com>
2022-01-14 22:59:32 +0100

    correct venv check

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ try:
     print(np.array([1,2]))
     print('numpy is installed')
     try:
-        a = plt()
+        a = plt.figure()
         print('you are not in a virtual environment')
     except:
         print('you are in a virtual environment :)')
