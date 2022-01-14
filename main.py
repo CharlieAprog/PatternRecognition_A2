8c575e94173553a0a1f1674c97fcb501eae340f6
CharlieAprog <charlie.albietz.uni@gmail.com>
2022-01-14 22:58:38 +0100

    venv check on main

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 import numpy as np
+import matplotlib.pyplot as plt
 ## test virtual env
-print(np.array([1,2]))
+try:
+    print(np.array([1,2]))
+    print('numpy is installed')
+    try:
+        a = plt()
+        print('you are not in a virtual environment')
+    except:
+        print('you are in a virtual environment :)')
+except:
+    print('numpy not yet installed')
