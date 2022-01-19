b74fe2737b726b0cc7d5ae51b7acf92e437184df
CharlieAprog <charlie.albietz.uni@gmail.com>
2022-01-19 14:27:54 +0100

    add clustering unfinished

--- src/numeric.py
+++ src/numeric.py
@@ -1,10 +1,17 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from tqdm import tqdm
+import random
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
+from sklearn.cluster import DBSCAN
+from sklearn.linear_model import LogisticRegression
+
+
+
 
 
 from sklearn.decomposition import PCA as sklearnPCA
@@ -12,8 +19,73 @@ from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
 from sklearn.metrics import accuracy_score, f1_score
 
 
+
+def make_scatter_plot(data, labels, title, save_plot=False, dimension=2, cluster=None):
+    if not any(cluster):
+        classes = list(set(np.array(labels.values)))
+    fig = plt.figure()
+    if dimension == 3:
+        ax = fig.add_subplot(projection='3d')
+        if any(cluster):
+            ax.scatter(data[0], data[1], data[2], c=cluster)
+        else:
+            for class_ in classes:
+                ax.scatter(data[labels==class_][0], data[labels==class_][1], data[labels==class_][2], label=class_)
+    else:
+        ax = fig.add_subplot()
+        if any(cluster):
+            ax.scatter(data[0], data[1], c=cluster)
+        else:
+            for class_ in classes:
+                ax.scatter(data[labels==class_][0], data[labels==class_][1], label=class_)
+
+    plt.title(title)
+    plt.legend()
+    if save_plot:
+        plt.savefig(f'../plots/numeric/{title}.jpg')
+    else:
+        plt.show()
+
+def visualise_data(x, y, dims=2, save=False, clusters=None):
+    title = 'visualisation of numeric data'
+    pca = sklearnPCA(n_components=dims) #2-dimensional PCA
+    pca_transformed = pd.DataFrame(pca.fit_transform(x))
+    if not any(clusters):
+        pca_transformed.index = y.index
+    make_scatter_plot(pca_transformed, y,f'{title}_PCA', dimension=dims, save_plot=save, cluster=clusters)
+
+    lda = LDA(n_components=dims) #2-dimensional LDA
+    lda_transformed = pd.DataFrame(lda.fit_transform(x, y))
+    if not any(cluster):
+        lda_transformed.index = y.index
+    make_scatter_plot(lda_transformed, y,f'{title}_LDA',dimension=dims, save_plot=save, cluster=clusters)
+
+def preprocess_data(model, x_train, x_test, y_train):
+    x_train = model.fit_transform(x_train, y_train)
+    x_test = model.transform(x_test)
+    return x_train, x_test
+
+def determine_best_col_num(model, classifier, x_train, y_train):
+    x_train, x_test, y_train, y_test = train_test_split(x_train, y_train, test_size=0.3, stratify=y_train)
+    print(x_train.shape, x_test.shape)
+    f1_scores = []
+    num_cols = range(2, 300)
+    for n in tqdm(num_cols):
+        model.set_params(n_components=n)
+        x_train_new, x_test_new = preprocess_data(model, x_train, x_test, y_train)
+        pred = run_model(classifier, x_train_new, x_test_new, y_train)
+        acc, f1 = get_score(pred, y_test)
+        f1_scores.append(f1)
+        print(n)
+
+    index = np.argmax(np.array(f1_scores))
+    print(f'smallest n:{num_cols[index]} with score of:{f1_scores[index]}')
+    plt.figure()
+    plt.plot(num_cols, f1_scores)
+    plt.show()
+
 def run_model(model, x_train, x_test, y_train):
-    model.fit(x_train, y_train)
+    model.fit(x_train, y_train.to_numpy())
     return model.predict(x_test)
 
 def classify_data(x_train, x_test, y_train, y_test):
@@ -29,112 +101,94 @@ def classify_data(x_train, x_test, y_train, y_test):
     y_predForest = run_model(Forest, x_train, x_test, y_train)
     forest_accuracy, forest_f1 = get_score(y_predForest, y_test)
 
-    print(f'knn accuracy:{knn_accuracy} knn f1:{knn_f1}')
-    print(f'tree accuracy:{tree_accuracy} tree f1:{tree_f1}')
-    print(f'forest accuracy:{forest_accuracy} forest f1:{forest_f1}')
-
-def test_data_reductions(x_train, x_test, y_train, y_test):
-    KNN = KNeighborsClassifier(n_neighbors=5)
-    pca_pred = run_model(KNN, x_train, x_test, y_train)
-    pca_accuracy = accuracy_score(y_test, pca_pred)
-    pca_f1 = f1_score(y_test, pca_pred, average="macro", zero_division=0)
-
-    lda_pred = run_model(KNN, x_train, x_test, y_train)
-    lda_accuracy = accuracy_score(y_test, lda_pred)
-    lda_f1 = f1_score(y_test, lda_pred, average="macro", zero_division=0)
-
-    print(f'pca accuracy: {pca_accuracy}, pca f1: {pca_f1}')
-    print(f'lda accuracy: {lda_accuracy}, lda f1: {lda_f1}')
-
-
-def preprocess_data(model, x_train, x_test, y_train):
-    x_train = model.fit_transform(x_train, y_train)
-    x_test = model.transform(x_test)
-    return x_train, x_test
-
-def visualise_data(x, y, dims=2, save=False):
-    title = 'visualisation of numeric data'
-    pca = sklearnPCA(n_components=dims) #2-dimensional PCA
-    pca_transformed = pd.DataFrame(pca.fit_transform(x))
-    pca_transformed.index = y.index
-    make_scatter_plot(pca_transformed, y,f'{title}_PCA', save_plot=save)
-
-    lda = LDA(n_components=dims) #2-dimensional LDA
-    lda_transformed = pd.DataFrame(lda.fit_transform(x, y))
-    lda_transformed.index = y.index
-    make_scatter_plot(lda_transformed, y,f'{title}_LDA', save_plot=save)
+    print(f'knn accuracy: {knn_accuracy}, knn f1: {knn_f1}')
+    print(f'tree accuracy: {tree_accuracy}, tree f1: {tree_f1}')
+    print(f'forest accuracy:{forest_accuracy}, forest f1: {forest_f1}')
 
 def get_score(pred, y_test):
-    accuracy = accuracy_score(y_test, pred)
-    f1 = f1_score(y_test, pred, average="macro", zero_division=0)
+    accuracy = round(accuracy_score(y_test, pred), 3)
+    f1 = round(f1_score(y_test, pred, average="macro", zero_division=0), 3)
     return accuracy, f1
 
-
-def make_scatter_plot(data, labels, title, save_plot=False, dimension=2):
-    classes = list(set(np.array(labels.values)))
-    fig = plt.figure()
-    if dimension == 3:
-        ax = fig.add_subplot(projection='3d')
-        for class_ in classes:
-            ax.scatter(data[labels==class_][0], data[labels==class_][1], data[labels==class_][2], label=class_)
-    else:
-        ax = fig.add_subplot()
-        for class_ in classes:
-            ax.scatter(data[labels==class_][0], data[labels==class_][1], label=class_)
-
-    plt.title(title)
-    plt.legend()
-    if save_plot:
-        plt.savefig(f'../plots/numeric/{title}.jpg')
-    else:
-        plt.show()
+def cluster_data(x_train, x_test):
+    complete_data = np.concatenate((x_train, x_test))
+    norm = (complete_data - np.min(complete_data))/np.ptp(complete_data)
+    eps_vals = np.arange(0.5, 7, 1)
+    min_vals = range(2, 6)
+    for eps_val in eps_vals:
+        for min_sample in min_vals:
+            clustering = DBSCAN(eps = eps_val, min_samples = min_sample).fit(complete_data)
+            labels = clustering.labels_
+            clusters =len(set(labels))-(1 if -1 in labels else 0)
+            print(clusters,eps_val,min_sample )
+    clustering = DBSCAN(eps = 2.5, min_samples = 4).fit(complete_data)
+    labels = clustering.labels_
+    clusters =len(set(labels))-(1 if -1 in labels else 0)
+    return labels, clusters
 
 
 def main():
+    random.seed(69)
     data_path = f'../data/Genes/Original'
-    print('reading data...')
-    x = pd.read_csv(f'{data_path}/data.csv').set_index('Unnamed: 0')
+    print('\nreading data...')
+
+    column_names = [f'gene_{i}' for i in range(0, 200)]
+    column_names.append('Unnamed: 0')
+    # x = pd.read_csv(f'{data_path}/data.csv').set_index('Unnamed: 0')
+    x = pd.read_csv(f'{data_path}/data.csv',usecols=column_names).set_index('Unnamed: 0')
     y = pd.read_csv(f'{data_path}/labels.csv').set_index('Unnamed: 0').Class
     row_size, col_size = x.shape
     unique_classes = list(set(np.array(y.values)))
     print(f'number of rows: {row_size}\nnumber of columns: {col_size}')
     print(f'unique classes: {unique_classes}')
     # visualise_data(x, y, save=False, dimensions=3)
-    x_train, x_test, y_train, y_test = train_test_split(
-            x, y, test_size=0.3, stratify=y
-        )
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, stratify=y)
 
-    print('preprocessing data...')
+    print('\npreprocessing data...')
     pca = sklearnPCA()
     lda = LDA()
-    pca_x_train, pca_x_test = preprocess_data(pca, x_train, x_test, y_train)
-    print(f'pca train before:{x_train.shape}, after:{pca_x_train.shape}')
-    print(f'pca test before:{x_test.shape}, after:{pca_x_test.shape}')
-
-    lda_x_train, lda_x_test = preprocess_data(pca, x_train, x_test, y_train)
-    print(f'lda train before:{x_train.shape}, after:{lda_x_train.shape}')
-    print(f'lda test before:{x_test.shape}, after:{lda_x_test.shape}')
-
-    print('testing preprocessed data')
     KNN = KNeighborsClassifier(n_neighbors=5)
-    original_pred = run_model(KNN, x_train, x_test, y_train)
-    original_accuracy = accuracy_score(y_test, original_pred)
-    original_f1 = f1_score(y_test, original_pred, average="macro", zero_division=0)
+    # determine_best_col_num(pca, KNN, x_train.to_numpy(), y_train)
+    # determine_best_col_num(lda, KNN, x_train.to_numpy(), y_train)
+
+
+
+
+    pca_x_train, pca_x_test = preprocess_data(pca, x_train, x_test, y_train)
+    print(f'columns before pca:{x_train.shape[1]}, columns after pca:{pca_x_train.shape[1]}')
+
+    lda_x_train, lda_x_test = preprocess_data(lda, x_train, x_test, y_train)
+    print(f'columns before lda:{x_train.shape[1]}, columns after lda:{lda_x_train.shape[1]}')
+
+
+    print('\ntesting preprocessed data')
+    original_train = x_train.to_numpy()
+    original_test = x_test.to_numpy()
+
+    original_pred = run_model(KNN, original_train, original_test, y_train)
+    original_accuracy, original_f1 = get_score(original_pred, y_test)
 
     pca_pred = run_model(KNN, pca_x_train, pca_x_test, y_train)
-    pca_accuracy = accuracy_score(y_test, pca_pred)
-    pca_f1 = f1_score(y_test, pca_pred, average="macro", zero_division=0)
+    pca_accuracy, pca_f1 = get_score(pca_pred, y_test)
 
-    lda_pred = run_model(KNN, pca_x_train, pca_x_test, y_train)
-    lda_accuracy = accuracy_score(y_test, lda_pred)
-    lda_f1 = f1_score(y_test, lda_pred, average="macro", zero_division=0)
+    lda_pred = run_model(KNN, lda_x_train, lda_x_test, y_train)
+    lda_accuracy, lda_f1 = get_score(lda_pred, y_test)
 
     print(f'original accuracy: {original_accuracy}, original f1: {original_f1}')
     print(f'pca accuracy: {pca_accuracy}, pca f1: {pca_f1}')
     print(f'lda accuracy: {lda_accuracy}, lda f1: {lda_f1}')
 
-    print('testing models')
+    print('\ntesting models...')
     classify_data(pca_x_train, pca_x_test, y_train, y_test)
+
+    print('\nclustering data...')
+    # original_labels, original_clusters= cluster_data(original_train, original_test)
+    reduced_labels, reduced_clusters = cluster_data(lda_x_train, lda_x_test)
+    # print(f'Estimated no. of clusters for original dataset: {original_clusters}')
+    print(f'Estimated no. of clusters for reduced dataset: {reduced_clusters}')
+    print(reduced_labels)
+    data = np.concatenate((lda_x_train, lda_x_test))
+    visualise_data(data, [], dims=3, save=False,  clusters=reduced_labels)
 
 
 
