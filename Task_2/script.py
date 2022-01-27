a3ce4caf7dfab67e1d806d6b4090d554b531683c
CharlieAprog <charlie.albietz.uni@gmail.com>
2022-01-27 10:59:31 +0100

    added task2 to man function

--- Task_2/script.py
+++ Task_2/script.py
@@ -26,52 +26,56 @@ def plot_it(res):
 def mean(list):
     return sum(list) / len(list)
 
-df = pd.read_csv('../Data/credit_cards/creditcard.csv')
-df = df.drop(['Time', 'Amount'], 1)
+def main():
+    df = pd.read_csv('../Data/credit_cards/creditcard.csv')
+    df = df.drop(['Time', 'Amount'], 1)
 
-X = df.drop('Class', 1).values
-y = df.Class.values
+    X = df.drop('Class', 1).values
+    y = df.Class.values
 
-n_reps = 10
-labeled_ratios = [0.01, 0.02, 0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.95, 0.98, 0.99]
+    n_reps = 10
+    labeled_ratios = [0.01, 0.02, 0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.95, 0.98, 0.99]
 
-per_lr = {}
-for labeled_ratio in labeled_ratios:
-    per_approach = {}
-    for approach in ['semisupervised', 'supervised', 'justlabelpropagation']:
-        print(labeled_ratio, approach)
-        ratio_corrects = [-1 for _ in range(n_reps)]
-        ratio_corrects_lab_prop = [-1 for _ in range(n_reps)]
-        for i in range(n_reps):
-            under_sampler = RandomUnderSampler()
-            X_balanced, y_balanced = under_sampler.fit_resample(X, y)
-            X_train, X_test, y_train, y_test = train_test_split(X_balanced, y_balanced, test_size= 0.2, shuffle= True)
+    per_lr = {}
+    for labeled_ratio in labeled_ratios:
+        per_approach = {}
+        for approach in ['semisupervised', 'supervised', 'justlabelpropagation']:
+            print(labeled_ratio, approach)
+            ratio_corrects = [-1 for _ in range(n_reps)]
+            ratio_corrects_lab_prop = [-1 for _ in range(n_reps)]
+            for i in range(n_reps):
+                under_sampler = RandomUnderSampler()
+                X_balanced, y_balanced = under_sampler.fit_resample(X, y)
+                X_train, X_test, y_train, y_test = train_test_split(X_balanced, y_balanced, test_size= 0.2, shuffle= True)
 
-            if approach == 'semisupervised' or approach == 'justlabelpropagation':
-                X_train_lab, X_train_unlab, y_train_lab = X_train[:round(labeled_ratio * len(X_train))], X_train[round(labeled_ratio * len(X_train)):], y_train[:round(labeled_ratio * len(y_train))]
-                label_prop_model = LabelPropagation()
-                label_prop_model.fit(X_train, np.append(y_train_lab, [-1 for _ in X_train_unlab]))
+                if approach == 'semisupervised' or approach == 'justlabelpropagation':
+                    X_train_lab, X_train_unlab, y_train_lab = X_train[:round(labeled_ratio * len(X_train))], X_train[round(labeled_ratio * len(X_train)):], y_train[:round(labeled_ratio * len(y_train))]
+                    label_prop_model = LabelPropagation()
+                    label_prop_model.fit(X_train, np.append(y_train_lab, [-1 for _ in X_train_unlab]))
 
-            if approach == 'justpropagation':
-                predicted = label_prop_model.predict(X_test)
-            
-            if approach == 'semisupervised':
-                y_train_unlab = label_prop_model.predict(X_train_unlab)
-                y_train = np.append(y_train_lab, y_train_unlab)
-            
-            if approach == 'supervised' or approach == 'semisupervised':
-                predictor = svm.SVC()
-                predictor.fit(X_train, y_train)
-                predicted = predictor.predict(X_test)
+                if approach == 'justpropagation':
+                    predicted = label_prop_model.predict(X_test)
 
-            ratio_correct = mean([predicted[i] == y_test[i] for i in range(len(predicted))])
-            ratio_corrects[i] = ratio_correct
+                if approach == 'semisupervised':
+                    y_train_unlab = label_prop_model.predict(X_train_unlab)
+                    y_train = np.append(y_train_lab, y_train_unlab)
 
-        per_approach[approach] = (mean(ratio_corrects), np.std(ratio_corrects), np.std(ratio_corrects) / np.sqrt(10))
-    per_lr[labeled_ratio] = per_approach
+                if approach == 'supervised' or approach == 'semisupervised':
+                    predictor = svm.SVC()
+                    predictor.fit(X_train, y_train)
+                    predicted = predictor.predict(X_test)
 
-# prints mean, standard deviation, and standard error for the success rates.
-for lr, pa in per_lr.items():
-    print(lr, pa)
+                ratio_correct = mean([predicted[i] == y_test[i] for i in range(len(predicted))])
+                ratio_corrects[i] = ratio_correct
 
-plot_it(per_lr)
+            per_approach[approach] = (mean(ratio_corrects), np.std(ratio_corrects), np.std(ratio_corrects) / np.sqrt(10))
+        per_lr[labeled_ratio] = per_approach
+
+    # prints mean, standard deviation, and standard error for the success rates.
+    for lr, pa in per_lr.items():
+        print(lr, pa)
+
+    plot_it(per_lr)
+
+if __name__ == '__main__':
+    main()
